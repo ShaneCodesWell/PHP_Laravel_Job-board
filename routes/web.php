758a5eb7069a556cee1bfa83848e008966989072
Shane Mensah <shanecodes69@gmail.com>
2024-01-23 13:15:30 +0000

    showing applied jobs by user

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,5 @@ Route::post('/jobs/apply', [App\Http\Controllers\Jobs\JobsController::class, 'ap
 
 Route::get('/categories/single/{name}', [App\Http\Controllers\Categories\CategoriesController::class, 'singleCategory'])->name('categories.single');
 
-
 Route::get('/users/profile/', [App\Http\Controllers\Users\UsersController::class, 'profile'])->name('profile');
+Route::get('/users/applications/', [App\Http\Controllers\Users\UsersController::class, 'applications'])->name('applications');
